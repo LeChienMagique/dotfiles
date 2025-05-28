3ab7e507738c49432f116155949922029837163a
Theo Prost <theo.prost@epita.fr>
2025-05-28 14:42:53 +0000

    Save wsl config

--- .emacs-custom.el
+++ .emacs-custom.el
@@ -3,22 +3,12 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(warning-suppress-log-types '((comp)))
- '(warning-suppress-types '((comp)))
- '(whitespace-style
-   '(face tabs spaces trailing space-before-tab newline indentation empty space-after-tab space-mark tab-mark))
- '(org-modules
-   '(org-bbdb org-bibtex org-docview org-gnus org-habit org-info org-irc org-mhe org-rmail org-w3m))
- '(eldoc-box-clear-with-C-g t)
- '(global-eldoc-mode t)
- '(pdf-latex-command "pdflatex")
- '(shell-escape-mode "-shell-escape")
- '(tex-start-options "--shell-escape")
  '(LaTeX-command "latex")
  '(TeX-command "tex")
  '(c-ts-mode-indent-offset 4)
  '(c-ts-mode-indent-style 'gnu)
  '(compilation-always-kill t)
+ '(compilation-search-path '(nil))
  '(custom-enabled-themes '(doom-challenger-deep))
  '(custom-safe-themes
    '("13096a9a6e75c7330c1bc500f30a8f4407bd618431c94aeab55c9855731a95e1" default))
@@ -26,12 +16,26 @@
  '(display-line-numbers 'relative)
  '(eldoc-box-clear-with-C-g t)
  '(electric-pair-mode t)
+ '(global-eldoc-mode t)
+ '(global-wakatime-mode t)
+ '(global-whitespace-mode t)
  '(inhibit-startup-screen t)
  '(make-backup-files nil)
+ '(org-modules
+   '(org-bbdb org-bibtex org-docview org-gnus org-habit org-info org-irc org-mhe org-rmail org-w3m))
  '(package-selected-packages
-   '(htmlize minesweeper nyan-mode docker writeroom-mode clang-format diff-hl eglot cypher-mode latex-preview-pane which-key cdlatex auctex company move-text graphviz-dot-mode nix-mode multiple-cursors magit simpc-mode smex ido-ubiquitous doom-themes doom-themes-theme dash-functional dash))
+   '(eglot gtags-mode dired-ranger wakatime-mode ahk-mode htmlize minesweeper nyan-mode docker writeroom-mode clang-format diff-hl cypher-mode latex-preview-pane which-key cdlatex auctex company move-text graphviz-dot-mode nix-mode multiple-cursors magit simpc-mode smex ido-ubiquitous doom-themes doom-themes-theme dash-functional dash))
+ '(pdf-latex-command "pdflatex")
  '(ring-bell-function 'ignore)
+ '(shell-escape-mode "-shell-escape")
+ '(tex-start-options "--shell-escape")
  '(visible-bell t)
+ '(wakatime-api-key "")
+ '(wakatime-cli-path "wakatime-cli")
+ '(warning-suppress-log-types '((comp)))
+ '(warning-suppress-types '((comp)))
+ '(whitespace-style
+   '(face trailing tabs spaces newline empty indentation space-after-tab space-before-tab space-mark))
  '(writeroom-fullscreen-effect 'maximized)
  '(writeroom-global-effects
    '(writeroom-set-alpha writeroom-set-menu-bar-lines writeroom-set-tool-bar-lines writeroom-set-vertical-scroll-bars writeroom-set-bottom-divider-width))
